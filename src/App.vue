b44a4b9da438e95b3efdf8c3d756df854b98b5d5
YoanMln <yoan.moulin@hotmail.fr>
2025-12-03 13:59:39 +0100

    first commit/add_views/citiesList

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import citiesList from "./views/citiesList.vue";
+
+const onListClick = () => {
+  alert("un utilisateur à cliqué :O !");
+};
+</script>
+
+<template>
+  <div class="app">
+    <h1>Weather</h1>
+    <button @click="onListClick">Cliquez !</button>
+    <citiesList></citiesList>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  background-color: rgba(255, 0, 0, 0.22);
+}
+</style>
---
+++ src/views/citiesList.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Weather - List of cities</h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: blue;
+}
+</style>
