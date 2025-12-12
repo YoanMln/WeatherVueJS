515eb5ded1533aefd1f134c8112e731aae3e41bc
YoanMln <yoan.moulin@hotmail.fr>
2025-12-12 14:32:38 +0100

    Add config for github actions

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/WeatherApp_VueJS/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
